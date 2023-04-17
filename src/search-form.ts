5e303251630e7734d2a4a9871133e9081e23e0cb
Nikita Gurskiy <supergurskiy@gmail.com>
2023-04-18 00:04:00 +0200

    изменил Search-form.ts

--- src/search-form.ts
+++ src/search-form.ts
@@ -1,6 +1,7 @@
-import { renderBlock } from './lib.js'
 
-export function renderSearchFormBlock (datecome, dateleave) {
+import { renderBlock } from './lib.js';
+
+export function renderSearchFormBlock(checkInDate: string, checkOutDate: string) {
   renderBlock(
     'search-form-block',
     `
@@ -12,19 +13,15 @@ export function renderSearchFormBlock (datecome, dateleave) {
             <input id="city" type="text" disabled value="Санкт-Петербург" />
             <input type="hidden" disabled value="59.9386,30.3141" />
           </div>
-          <!--<div class="providers">
-            <label><input type="checkbox" name="provider" value="homy" checked /> Homy</label>
-            <label><input type="checkbox" name="provider" value="flat-rent" checked /> FlatRent</label>
-          </div>--!>
         </div>
         <div class="row">
           <div>
             <label for="check-in-date">Дата заезда</label>
-            <input id="check-in-date" type="date" value="2023-04-10" min="2023-04-10" max="2023-05-31" name="checkin" />
+            <input id="check-in-date" type="date" value="${checkInDate}" min="2023-04-10" max="2023-05-31" name="checkin" />
           </div>
           <div>
             <label for="check-out-date">Дата выезда</label>
-            <input id="check-out-date" type="date" value="2023-04-12" min="2023-04-10" max="2023-05-31" name="checkout" />
+            <input id="check-out-date" type="date" value="${checkOutDate}" min="2023-04-10" max="2023-05-31" name="checkout" />
           </div>
           <div>
             <label for="max-price">Макс. цена суток</label>
@@ -37,5 +34,5 @@ export function renderSearchFormBlock (datecome, dateleave) {
       </fieldset>
     </form>
     `
-  )
+  );
 }
